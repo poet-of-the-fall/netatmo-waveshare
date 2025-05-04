242bbf8fadc40cb33b4c8b6ffe5aed7ac5a7deb5
Danilo Gasdzik <poet_of_the_fall@gmx.de>
2025-05-04 14:44:26 +0200

    update

--- widgets/ModuleWidget.py
+++ widgets/ModuleWidget.py
@@ -130,8 +130,7 @@ class MainModuleWidget(ModuleWidget):
         config = ConfigHelper()
         header = ""
         body = "?"
-        # if 'dashboard_data' in module and 'Humidity' in module['dashboard_data'] and 'CO2' in module['dashboard_data']:
-        if 'dashboard_data' in module:
+        if 'dashboard_data' in module and 'Humidity' in module['dashboard_data'] and 'CO2' in module['dashboard_data']:
             header = str(module['dashboard_data']['Humidity']) + "%, " + str(module['dashboard_data']['CO2']) + "ppm"
         if 'dashboard_data' in module and 'Temperature' in module['dashboard_data']:
             body = config.format_decimal(module['dashboard_data']['Temperature']) + u'\N{DEGREE SIGN}'
@@ -146,7 +145,8 @@ class OutdoorModuleWidget(ModuleWidget):
         config = ConfigHelper()
         header = ""
         body = "?"
-        if 'dashboard_data' in module and 'Humidity' in module['dashboard_data'] and 'Pressure' in module['dashboard_data']:
+        # if 'dashboard_data' in module and 'Humidity' in module['dashboard_data'] and 'Pressure' in module['dashboard_data']:
+        if 'dashboard_data' in module:
             header = str(module['dashboard_data']['Humidity']) + "%, " + config.format_decimal(main_module['dashboard_data']['Pressure']) + "mbar"
         if 'dashboard_data' in module and 'Temperature' in module['dashboard_data']:
             body = config.format_decimal(module['dashboard_data']['Temperature']) + u'\N{DEGREE SIGN}'
